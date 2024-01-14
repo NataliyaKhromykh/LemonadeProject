0ffd78eb3a139a2b4ac087bcf03e94b3e8a35e61
Nataliya Khromykh <nataliyakhromykh@Nataliyas-MBP.attlocal.net>
2024-01-14 18:36:22 -0500

    first commit

--- src/MainComponents/NewMenuContainer.js
+++ src/MainComponents/NewMenuContainer.js
@@ -36,7 +36,7 @@ const NewMenuContainer = () => {
         auto();
       }
       return () => clearInterval(slideInterval);
-    }, [card]);
+    }, [autoScroll, slideInterval, card]);
   
     return (
 
@@ -73,69 +73,6 @@ const NewMenuContainer = () => {
       </div>
     )
 
-    // const [currentSlide, setCurrentSlide] = useState(0);
-    // const slideLength = NewMenuContainerData.length;
-
-    // const autoScroll = true;
-    // let slideInterval;
-    // let intervalTime = 5000;
-
-    // const nextSlide = () => {
-    //     setCurrentSlide(currentSlide === slideLength -1 ? 0 : currentSlide + 1);
-    // };
-    // const prevSlide = () => {
-    //     setCurrentSlide(currentSlide === 0 ? slideLength -1 : currentSlide - 1);
-    // };
-    // function auto() {
-    //     slideInterval = setInterval(nextSlide,intervalTime)
-    // }
-
-    // useEffect(() =>{
-    //     if (autoScroll) {
-    //         auto()
-    //     }
-    //     return () => clearInterval(slideInterval);
-    // },[currentSlide])
-
-
-    // return (
-    //     <div className="newMenuContainer">
-    //     <AiOutlineArrowLeft className="menuArrowTwo prev" onClick={prevSlide}/>
-    //         <div>
-    //             <div className="container">
-    //             <h1 className="newMenuContainerText">New this week:</h1>
-    //             </div>
-    //             <div className="ourNewMenuSlider">
-    //             {NewMenuContainerData.map((slide, index) => {
-    //                 return (
-    //                     <div key={index} className={index === currentSlide ? "slidemenu currentmenu" : "slidemenu"}>
-    //                         {index === currentSlide && (
-    //                             <div className="newMenuBox">
-    //                                 <div className="newMenuMiniBox">
-    //                                 <img className="newMenuPhoto" src={slide.promo} alt="menuitem"/>
-    //                                 <p className="itemName"> {slide.promoName} </p>
-    //                                 </div>
-    //                                 <div className="newMenuMiniBox">
-    //                                 <img className="newMenuPhoto" src={slide.promo}  alt="menuitem"/>
-    //                                 <p className="itemName"> {slide.promoName} </p>
-    //                                 </div>
-    //                                 <div className="newMenuMiniBox">
-    //                                 <img className="newMenuPhoto" src={slide.promo} alt="menuitem"/>
-    //                                 <p className="itemName"> {slide.promoName} </p>
-    //                                 </div>
-    //                             </div>
-    //                         )}
-    //                     </div>
-    //                 )
-    //             })}
-    //             </div>
-    //             <div className="container">
-    //             <div className="newMenuContainerText"><h3>Find out <Link className="moreLink" to="/menu">more</Link></h3></div>
-    //         </div>
-    //         </div>
-    //         <AiOutlineArrowRight className="menuArrowTwo next" onClick={nextSlide}/>
-    //     </div>
-    // )
 }
 
 
